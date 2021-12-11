f6fbede7cabb9e709086b779c1cacece2aa79d2d
NickitaKraev <75929318+NickitaKraev@users.noreply.github.com>
2021-12-11 12:11:56 +0300

    Add files via upload

---
+++ modules/task_1/kraev_n_number_of_alternating_signs/number_of_alternating_signs.h
@@ -0,0 +1,11 @@
+// Copyright 2021 Kraev Nikita
+#ifndef MODULES_TASK_1_KRAEV_N_NUMBER_OF_ALTERNATING_SIGNS_NUMBER_OF_ALTERNATING_SIGNS_H_
+#define MODULES_TASK_1_KRAEV_N_NUMBER_OF_ALTERNATING_SIGNS_NUMBER_OF_ALTERNATING_SIGNS_H_
+
+#include <vector>
+
+std::vector<int> getRandomVector(const int size);
+int findingNumberOfSignAlternations(const std::vector<int>& vec);
+int parallelFindingNumberOfSignAlternations(const std::vector<int>& vec, const int size);
+
+#endif  //MODULES_TASK_1_KRAEV_N_NUMBER_OF_ALTERNATING_SIGNS_NUMBER_OF_ALTERNATING_SIGNS_H_
