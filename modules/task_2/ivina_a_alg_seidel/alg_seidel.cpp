3c4161ff84a8dab5e9fc634fb0aa0b54cd9f2166
Anastasya Ivina <adelinesommers@gmail.com>
2021-12-17 00:26:19 +0300

    Fix review issues

--- modules/task_2/ivina_a_alg_seidel/alg_seidel.cpp
+++ modules/task_2/ivina_a_alg_seidel/alg_seidel.cpp
@@ -1,154 +1,130 @@
 // Copyright 2021 Ivina Anastasiya
-#include "./alg_seidel.h"
+#include "alg_seidel.h"
 #include <mpi.h>
-#include <vector>
 #include <random>
+#include <vector>
+
 
 double randomValue(int min, int max) {
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> range(min, max);
-	return range(rd);
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> range(min, max);
+  return range(rd);
 }
 
-std::vector<double> seqSeidel(std::vector<std::vector<double>> &a, std::vector<double> &b, double eps) {
-	int n;
-	std::vector<double> x;
-	std::vector<double> p;
-	n = b.size();
+std::vector<double> seqSeidel(std::vector<std::vector<double>> &a,
+                              std::vector<double> &b, double eps) {
+  int n;
+  std::vector<double> x;
+  std::vector<double> p;
+  n = b.size();
 
-	for (int i = 0; i < n; i++) {
-		x.push_back(1.0F);
-		p.push_back(0.0F);
-	}
+  for (int i = 0; i < n; i++) {
+    x.push_back(1.0F);
+    p.push_back(0.0F);
+  }
 
-	auto is_diagonal = [&]() -> bool {
-		bool is_diag = true;
-		for (int i = 0; i < n; i++) {
-			double sum = 0;
-			for (const auto e : a[i]) {
-				sum += abs(e);
-			}
-			const auto diag = a[i][i];
-			sum -= diag;
-			if (sum > diag) {
-				is_diag = false;
-			}
-		}
-		return is_diag;
-	};
+  auto is_diagonal = [&]() -> bool {
+    bool is_diag = true;
+    for (int i = 0; i < n; i++) {
+      double sum = 0;
+      for (const auto e : a[i]) {
+        sum += abs(e);
+      }
+      const auto diag = a[i][i];
+      sum -= diag;
+      if (sum > diag) {
+        is_diag = false;
+      }
+    }
+    return is_diag;
+  };
 
-	if (is_diagonal()) {
-		double Fullsum = 0;
-		do {
-			p = x;
-			double norm = 0;
-			for (int i = 0; i < n; i++) {
-				double sum = 0;
-				for (int j = 0; j < n; j++) {
-					if (j != i) {
-						sum += (a[i][j] * x[j]);
-					}
-				}
-				x[i] = (b[i] - sum) / a[i][i];
-				norm += (x[i] - p[i]) * (x[i] - p[i]);
-				Fullsum = norm;
-			}
-		} while (!(sqrt(Fullsum) < eps));
-
-		auto result = [=](double x) -> double {
-			int i = 0;
-			auto new_eps = eps;
-			while (new_eps < 1) {
-				i++;
-				new_eps *= 10;
-			}
-			const auto okr = pow(10.0F, i);
-			x = int(x * okr + 0.5) / double(okr);
-			return x;
-		};
-		return x;
-	}
+  if (is_diagonal()) {
+    double Fullsum = 0;
+    do {
+      p = x;
+      double norm = 0;
+      for (int i = 0; i < n; i++) {
+        double sum = 0;
+        for (int j = 0; j < n; j++) {
+          if (j != i) {
+            sum += (a[i][j] * x[j]);
+          }
+        }
+        x[i] = (b[i] - sum) / a[i][i];
+        norm += (x[i] - p[i]) * (x[i] - p[i]);
+        Fullsum = norm;
+      }
+    } while (!(sqrt(Fullsum) < eps));
+  }
+  return x;
 }
 
-std::vector<double> parallSeidel(std::vector<std::vector<double>> &a, std::vector<double> &b, double eps) {
-	int size, rank;
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	int n;
+std::vector<double> parallSeidel(std::vector<std::vector<double>> &a,
+                                 std::vector<double> &b, double eps) {
+  int size, rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  int n;
 
-	std::vector<double> x;
-	std::vector<double> p;
-	n = b.size();
-	for (int i = 0; i < n; i++) {
-		x.push_back(1.0F);
-		p.push_back(0.0F);
-	}
+  std::vector<double> x;
+  std::vector<double> p;
+  n = b.size();
+  for (int i = 0; i < n; i++) {
+    x.push_back(1.0F);
+    p.push_back(0.0F);
+  }
 
-	auto is_diagonal = [&]() -> bool {
-		bool is_diag = true;
-		for (int i = 0; i < n; i++) {
-			double sum = 0;
-			for (const auto e : a[i]) {
-				sum += abs(e);
-			}
-			const auto diag = a[i][i];
-			sum -= diag;
-			if (sum > diag) {
-				is_diag = false;
-			}
-		}
-		return is_diag;
-	};
+  auto is_diagonal = [&]() -> bool {
+    bool is_diag = true;
+    for (int i = 0; i < n; i++) {
+      double sum = 0;
+      for (const auto e : a[i]) {
+        sum += abs(e);
+      }
+      const auto diag = a[i][i];
+      sum -= diag;
+      if (sum > diag) {
+        is_diag = false;
+      }
+    }
+    return is_diag;
+  };
 
-	if (is_diagonal()) {
-		double fullSum = 0;
-		int num = (n / size)*(size - 1) + ((n - (n / size)*size) - 1);
-		do {
-			p = x;
-			double norm = 0;
-			for (int i = rank; i < n; i += size) {
-				double sum = 0;
-				for (int j = 0; j < n; j++) {
-					if (j != i) {
-						sum += (a[i][j] * x[j]);
-					}
-				}
-				x[i] = (b[i] - sum) / a[i][i];
-				norm += (x[i] - p[i]) * (x[i] - p[i]);
-				if (rank == 0) {
-					for (int j = 0; j < num; j++) {
+  if (is_diagonal()) {
+    double fullSum = 0;
+    int num = (n / size) * (size - 1) + ((n - (n / size) * size) - 1);
+    do {
+      p = x;
+      double norm = 0;
+      for (int i = rank; i < n; i += size) {
+        double sum = 0;
+        for (int j = 0; j < n; j++) {
+          if (j != i) {
+            sum += (a[i][j] * x[j]);
+          }
+        }
+        x[i] = (b[i] - sum) / a[i][i];
+        norm += (x[i] - p[i]) * (x[i] - p[i]);
+        if (rank == 0) {
+          for (int j = 0; j < num; j++) {
 
-						double buff[2];
-						MPI_Recv(buff, 2, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
-						x[static_cast<size_t>(buff[0])] = buff[1];
-					}
-
-				}
-				else {
-					double buff[2];
-					buff[0] = i;
-					buff[1] = x[i];
-					MPI_Send(buff, 2, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-				}
-			}
-			MPI_Reduce(&norm, &fullSum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-			MPI_Bcast(&fullSum, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		} while (!(sqrt(fullSum) < eps));
-
-		if (rank == 0) {
-			auto result = [=](double x) -> double {
-				int i = 0;
-				auto new_eps = eps;
-				while (new_eps < 1) {
-					i++;
-					new_eps *= 10;
-				}
-				const auto okr = pow(10.0F, i);
-				x = int(x * okr + 0.5) / double(okr);
-				return x;
-			};
-		}
-		return x;
-	}
+            double buff[2];
+            MPI_Recv(buff, 2, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD,
+                     MPI_STATUSES_IGNORE);
+            x[static_cast<size_t>(buff[0])] = buff[1];
+          }
+        } else {
+          double buff[2];
+          buff[0] = i;
+          buff[1] = x[i];
+          MPI_Send(buff, 2, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+        }
+      }
+      MPI_Reduce(&norm, &fullSum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+      MPI_Bcast(&fullSum, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    } while (!(sqrt(fullSum) < eps));
+  }
+  return x;
 }
--- modules/task_2/ivina_a_alg_seidel/main.cpp
+++ modules/task_2/ivina_a_alg_seidel/main.cpp
@@ -1,137 +1,166 @@
 // Copyright 2021 Ivina Anastasiya
-#include "./alg_seidel.h"
-#include <gtest/gtest.h>
+#include "alg_seidel.h"
 #include <gtest-mpi-listener.hpp>
+#include <gtest/gtest.h>
+
 
 TEST(MPI, constnum3) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<std::vector<double>> a;
-	std::vector<double> b;
-	double eps = 0.01;
-	if (rank == 0) {
-		a = {
-		{10, 1, 1},
-		{2, 10, 1},
-		{2, 2, 10}
-		};
-		b = { 12, 13, 14 };
-	}		
-	std::vector<double> res = parallSeidel(a, b, eps);
-	if (rank == 0) {
-		ASSERT_NEAR(res[0], 1, 0.01);
-		ASSERT_NEAR(res[1], 1, 0.01);
-		ASSERT_NEAR(res[2], 1, 0.01);
-	}
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<std::vector<double>> a;
+  std::vector<double> b;
+  double eps = 0.01;
+  if (rank == 0) {
+    a = {{10, 1, 1}, {2, 10, 1}, {2, 2, 10}};
+    b = {12, 13, 14};
+  }
+  std::vector<double> res = parallSeidel(a, b, eps);
+  if (rank == 0) {
+    ASSERT_NEAR(res[0], 1, 0.01);
+    ASSERT_NEAR(res[1], 1, 0.01);
+    ASSERT_NEAR(res[2], 1, 0.01);
+  }
 }
 
 TEST(MPI, randnum3) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<std::vector<double>> a;
-	std::vector<double> b;
-	double eps = 0.01;
-	int n = 3;
-	double diag = randomValue(101, 150);
-	
-	if (rank == 0) {
-		b.clear();
-		for (int i = 0; i < n; i++) {
-			std::vector<double> row;
-			for (int j = 0; j < n; j++) {
-				row.push_back(randomValue(0, 50));
-			}
-			a.push_back(row);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<std::vector<double>> a;
+  std::vector<double> b;
+  double eps = 0.01;
+  int n = 3;
+  double diag = randomValue(101, 150);
 
-			a[i][i] = diag;
-			b.push_back(randomValue(0, 50));
-		}
-	}
+  if (rank == 0) {
+    b.clear();
+    for (int i = 0; i < n; i++) {
+      std::vector<double> row;
+      for (int j = 0; j < n; j++) {
+        row.push_back(randomValue(0, 50));
+      }
+      a.push_back(row);
 
-	std::vector<double> res = parallSeidel(a, b, eps);
-	if (rank == 0) {
-		std::vector<double> seq_res = seqSeidel(a, b, eps);
-		for (int i = 0; i < n; i++) {
-			ASSERT_EQ(seq_res[i], res[i]);
-		}
-	}
+      a[i][i] = diag;
+      b.push_back(randomValue(0, 50));
+    }
+  }
+
+  std::vector<double> res = parallSeidel(a, b, eps);
+  if (rank == 0) {
+    std::vector<double> seq_res = seqSeidel(a, b, eps);
+    for (int i = 0; i < n; i++) {
+      ASSERT_EQ(seq_res[i], res[i]);
+    }
+  }
+}
+
+TEST(MPI, randnum1) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<std::vector<double>> a;
+  std::vector<double> b;
+  double eps = 0.01;
+  int n = 1;
+  double diag = randomValue(101, 150);
+
+  if (rank == 0) {
+    b.clear();
+    for (int i = 0; i < n; i++) {
+      std::vector<double> row;
+      for (int j = 0; j < n; j++) {
+        row.push_back(randomValue(0, 50));
+      }
+      a.push_back(row);
+
+      a[i][i] = diag;
+      b.push_back(randomValue(0, 50));
+    }
+  }
+
+  std::vector<double> res = parallSeidel(a, b, eps);
+  if (rank == 0) {
+    std::vector<double> seq_res = seqSeidel(a, b, eps);
+    for (int i = 0; i < n; i++) {
+      ASSERT_EQ(seq_res[i], res[i]);
+    }
+  }
 }
 
 TEST(MPI, randnum10) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<std::vector<double>> a;
-	std::vector<double> b;
-	double eps = 0.01;
-	int n = 10;
-	double diag = randomValue(101, 150);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<std::vector<double>> a;
+  std::vector<double> b;
+  double eps = 0.01;
+  int n = 10;
+  double diag = randomValue(101, 150);
 
-	if (rank == 0) {
-		b.clear();
-		for (int i = 0; i < n; i++) {
-			std::vector<double> row;
-			for (int j = 0; j < n; j++) {
-				row.push_back(randomValue(0, 50));
-			}
-			a.push_back(row);
+  if (rank == 0) {
+    b.clear();
+    for (int i = 0; i < n; i++) {
+      std::vector<double> row;
+      for (int j = 0; j < n; j++) {
+        row.push_back(randomValue(0, 50));
+      }
+      a.push_back(row);
 
-			a[i][i] = diag;
-			b.push_back(randomValue(0, 50));
-		}
-	}
+      a[i][i] = diag;
+      b.push_back(randomValue(0, 50));
+    }
+  }
 
-	std::vector<double> res = parallSeidel(a, b, eps);
-	if (rank == 0) {
-		std::vector<double> seq_res = seqSeidel(a, b, eps);
-		for (int i = 0; i < n; i++) {
-			ASSERT_EQ(seq_res[i], res[i]);
-		}
-	}
+  std::vector<double> res = parallSeidel(a, b, eps);
+  if (rank == 0) {
+    std::vector<double> seq_res = seqSeidel(a, b, eps);
+    for (int i = 0; i < n; i++) {
+      ASSERT_EQ(seq_res[i], res[i]);
+    }
+  }
 }
 
 TEST(MPI, randnum50) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<std::vector<double>> a;
-	std::vector<double> b;
-	double eps = 0.01;
-	int n = 50;
-	double diag = randomValue(101, 150);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<std::vector<double>> a;
+  std::vector<double> b;
+  double eps = 0.01;
+  int n = 50;
+  double diag = randomValue(101, 150);
 
-	if (rank == 0) {
-		b.clear();
-		for (int i = 0; i < n; i++) {
-			std::vector<double> row;
-			for (int j = 0; j < n; j++) {
-				row.push_back(randomValue(0, 50));
-			}
-			a.push_back(row);
+  if (rank == 0) {
+    b.clear();
+    for (int i = 0; i < n; i++) {
+      std::vector<double> row;
+      for (int j = 0; j < n; j++) {
+        row.push_back(randomValue(0, 50));
+      }
+      a.push_back(row);
 
-			a[i][i] = diag;
-			b.push_back(randomValue(0, 50));
-		}
-	}
+      a[i][i] = diag;
+      b.push_back(randomValue(0, 50));
+    }
+  }
 
-	std::vector<double> res = parallSeidel(a, b, eps);
-	if (rank == 0) {
-		std::vector<double> seq_res = seqSeidel(a, b, eps);
-		for (int i = 0; i < n; i++) {
-			ASSERT_EQ(seq_res[i], res[i]);
-		}
-	}
+  std::vector<double> res = parallSeidel(a, b, eps);
+  if (rank == 0) {
+    std::vector<double> seq_res = seqSeidel(a, b, eps);
+    for (int i = 0; i < n; i++) {
+      ASSERT_EQ(seq_res[i], res[i]);
+    }
+  }
 }
 
-int main(int argc, char** argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	MPI_Init(&argc, &argv);
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  MPI_Init(&argc, &argv);
 
-	::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
-	::testing::TestEventListeners& listeners =
-		::testing::UnitTest::GetInstance()->listeners();
+  ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+  ::testing::TestEventListeners &listeners =
+      ::testing::UnitTest::GetInstance()->listeners();
 
-	listeners.Release(listeners.default_result_printer());
-	listeners.Release(listeners.default_xml_generator());
+  listeners.Release(listeners.default_result_printer());
+  listeners.Release(listeners.default_xml_generator());
 
-	listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
-	return RUN_ALL_TESTS();
+  listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+  return RUN_ALL_TESTS();
 }
