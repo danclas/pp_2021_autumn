d61c88c05183c26e5380a707608e69373a271f4a
Kcenia <Kcenia4010@gmail.com>
2021-12-18 02:33:28 +0300

    Fix style

--- modules/task_3/zaitseva_k_conjugate_gradient_method/conjugateGradientMethod.h
+++ modules/task_3/zaitseva_k_conjugate_gradient_method/conjugateGradientMethod.h
@@ -2,13 +2,15 @@
 #ifndef MODULES_TASK_1_ZAITSEVA_K_CONJUGATE_GRADIENT_METHOD_H_
 #define MODULES_TASK_1_ZAITSEVA_K_CONJUGATE_GRADIENT_METHOD_H_
 #include <mpi.h>
-#include <vector>
+
 #include <iostream>
 #include <random>
+#include <vector>
 
 void RandomVec(int n, double* A);
 double ScalarMult(std::vector<double> x, std::vector<double> y);
 std::vector<double> conjugateGradientMethodSerial(double* A, double* b, int n);
-std::vector<double>  conjugateGradientMethodParallel(double* A, double* b, int n);
+std::vector<double> conjugateGradientMethodParallel(double* A, double* b,
+                                                    int n);
 
 #endif  // MODULES_TASK_1_ZAITSEVA_K_CONJUGATE_GRADIENT_METHOD_H_
