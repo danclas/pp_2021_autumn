311bef25cc98f09b5af6099962bf975829e65844
Kcenia <Kcenia4010@gmail.com>
2021-12-18 02:17:58 +0300

    Initial commit

---
+++ modules/task_3/zaitseva_k_conjugate_gradient_method/conjugateGradientMethod.h
@@ -0,0 +1,14 @@
+// Copyright 2021 Zaitseva Ksenia
+#ifndef MODULES_TASK_1_ZAITSEVA_K_CONJUGATE_GRADIENT_METHOD_H_
+#define MODULES_TASK_1_ZAITSEVA_K_CONJUGATE_GRADIENT_METHOD_H_
+#include <mpi.h>
+#include <vector>
+#include <iostream>
+#include <random>
+
+void RandomVec(int n, double* A);
+double ScalarMult(std::vector<double> x, std::vector<double> y);
+std::vector<double> conjugateGradientMethodSerial(double* A, double* b, int n);
+std::vector<double>  conjugateGradientMethodParallel(double* A, double* b, int n);
+
+#endif  // MODULES_TASK_1_ZAITSEVA_K_CONJUGATE_GRADIENT_METHOD_H_
