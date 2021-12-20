4e2186cbe177cf91a5ad6c45e7a5226f06f0c54d
ZILLeF13 <71269813+ZILLeF13@users.noreply.github.com>
2021-12-20 18:37:22 +0300

    first commit

---
+++ modules/task_2/kirillov_n_gauss_vertically/gauss_vertically.h
@@ -0,0 +1,9 @@
+// Copyright 2021 Kirillov Nikita
+#ifndef MODULES_TASK_2_KIRILLOV_N_GAUSS_VERTICALLY_GAUSS_VERTICALLY_H_
+#define MODULES_TASK_2_KIRILLOV_N_GAUSS_VERTICALLY_GAUSS_VERTICALLY_H_
+
+double* getRandomMatrix(int width, int height);
+double* getSequentialOperations(double* _matrix, int width, int height);
+double* getParallelOperations(double* matrix, int widht, int height);
+
+#endif  // MODULES_TASK_2_KIRILLOV_N_GAUSS_VERTICALLY_GAUSS_VERTICALLY_H_
