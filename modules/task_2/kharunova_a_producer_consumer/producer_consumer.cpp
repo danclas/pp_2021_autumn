860a0e60bc5e5e42e50a3ff9270c84b6b59373a9
Zhigulyaka <alina.harunova@mail.ru>
2021-12-20 21:17:24 +0300

    Refactoring

--- modules/task_2/kharunova_a_producer_consumer/producer_consumer.cpp
+++ modules/task_2/kharunova_a_producer_consumer/producer_consumer.cpp
@@ -1,62 +1,44 @@
 // Copyright 2021 Kharunova Alina
 #include <mpi.h>
 
-#include <algorithm>
-#include <ctime>
 #include <iostream>
-#include <queue>
+#include <ctime>
+#include <stack>
 #include <random>
-#include <string>
 
 #include "../../../modules/task_2/kharunova_a_producer_consumer/producer_consumer.h"
 
-double waitingTime = 0.01;
-
-void waiting(double seconds) {
-  double time = MPI_Wtime();
-  while (MPI_Wtime() - time < seconds) {
-  }
-}
-
-int process(int numberOfElements) {
-  int size, rank;
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  MPI_Status status;
+int process(int numberOfEnterings) {
+  int element, rank, emptyConsumer = -1;
+  MPI_Status proc;
   MPI_Request request;
-  int data;
-  int emptyConsumer = -1;
 
-  std::queue<int> buffer;
-  while (numberOfElements != 0) {
+  std::stack<int> buffer;
+  while (numberOfEnterings != 0) {
     if (emptyConsumer == -1) {
-      MPI_Recv(&data, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD,
-               &status);
+      MPI_Recv(&rank, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD,
+               &proc);
     } else {
-      MPI_Recv(&data, 1, MPI_INT, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, &status);
+      MPI_Recv(&rank, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &proc);
     }
-    --numberOfElements;
-    if (status.MPI_TAG == 3) {
-      buffer.push(data);
+    if (rank % 2 == 1) {
+      buffer.push(rank);
       if (emptyConsumer != -1) {
-        int el = buffer.front();
+        element = buffer.top();
         buffer.pop();
-        MPI_Isend(&el, 1, MPI_INT, emptyConsumer, 4, MPI_COMM_WORLD, &request);
-        MPI_Request_free(&request);
+        MPI_Send(&element, 1, MPI_INT, emptyConsumer, 1, MPI_COMM_WORLD);
         emptyConsumer = -1;
       }
     } else {
       if (!buffer.empty()) {
-        int element = buffer.front();
+        element = buffer.top();
         buffer.pop();
-        MPI_Isend(&element, 1, MPI_INT, status.MPI_SOURCE, 4, MPI_COMM_WORLD,
-                  &request);
-        MPI_Request_free(&request);
+        MPI_Send(&element, 1, MPI_INT, proc.MPI_SOURCE, 1, MPI_COMM_WORLD);
       } else {
-        emptyConsumer = status.MPI_SOURCE;
+        emptyConsumer = proc.MPI_SOURCE;
       }
     }
+    numberOfEnterings--;
   }
   return buffer.size();
 }
@@ -65,17 +47,24 @@ void producer() {
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  waiting(waitingTime * rank);
-  MPI_Send(&rank, 1, MPI_INT, 0, 3, MPI_COMM_WORLD);
+  waiting();
+  MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
 }
 
 void consumer() {
-  int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  waiting(waitingTime * rank);
-  MPI_Send(&rank, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
-
-  int element;
   MPI_Status status;
-  MPI_Recv(&element, 1, MPI_INT, 0, 4, MPI_COMM_WORLD, &status);
+  int element, rank;
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  waiting();
+  MPI_Send(&rank, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+  MPI_Recv(&element, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+}
+
+void waiting() {
+  std::random_device dev;
+  std::mt19937 gen(dev());
+  double time = gen() % 1, now = MPI_Wtime();
+  while (MPI_Wtime() < now + time) {
+  }
 }
