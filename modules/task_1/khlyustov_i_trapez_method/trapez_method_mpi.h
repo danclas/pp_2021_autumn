97f6d067d7b62b5f47c5807997b5a1027aeb4518
MaksaLegend <47770844+Riladavin@users.noreply.github.com>
2021-12-20 17:30:16 +0300

    update to new check

--- modules/task_1/khlyustov_i_trapez_method/trapez_method_mpi.h
+++ modules/task_1/khlyustov_i_trapez_method/trapez_method_mpi.h
@@ -1,7 +1,6 @@
 // Copyright 2021 Khlyustov Ilya
 #ifndef MODULES_TASK_1_KHLYUSTOV_I_TRAPEZ_METHOD_TRAPEZ_METHOD_MPI_H_
 #define MODULES_TASK_1_KHLYUSTOV_I_TRAPEZ_METHOD_TRAPEZ_METHOD_MPI_H_
-
 #include <mpi.h>
 #include <cmath>
 #include <functional>
