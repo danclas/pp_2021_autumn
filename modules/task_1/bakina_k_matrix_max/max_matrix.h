1332a0150e166598b483ec4052db4a5c5916f7eb
BakinaKA <backina.xen@yandex.ru>
2021-12-21 03:56:02 +0300

    fix segfault

--- modules/task_1/bakina_k_matrix_max/max_matrix.h
+++ modules/task_1/bakina_k_matrix_max/max_matrix.h
@@ -1,9 +1,10 @@
 // Copyright 2021 Bakina Ksenia
 #ifndef MODULES_TASK_1_BAKINA_K_MATRIX_MAX_MAX_MATRIX_H_
 #define MODULES_TASK_1_BAKINA_K_MATRIX_MAX_MAX_MATRIX_H_
+#include <vector>
 
-void get_random_matrix(int** mtrx, const int row_count, const int column_count);
-int linear_search_max_el(const int* sub_matrix, const int size);
-int parallel_search_max_el(int** matrix, int row_count, int column_count);
+void get_random_matrix(std::vector<std::vector<int>>* mtrx, const int row_count, const int column_count);
+int linear_search_max_el(std::vector<int>* sub_matrix, const int size);
+int parallel_search_max_el(std::vector<std::vector<int>>* matrix, int row_count, int column_count);
 
 #endif  // MODULES_TASK_1_BAKINA_K_MATRIX_MAX_MAX_MATRIX_H_
