1e44c73fa6d5f30ebcb8a17adf4dc159e5366c82
Chesnok43 <sovetoveg@mail.ru>
2021-12-23 16:50:19 +0300

    fir

---
+++ modules/task_1/sovetov_e_close_value_vector/close_value_vector.h
@@ -0,0 +1,13 @@
+
+
+// Copyright 2021 Sovetov Egor
+#ifndef MODULES_TASK_1_SOVETOV_E_CLOSE_VALUE_VECTOR_CLOSE_VALUE_VECTOR_H_
+#define MODULES_TASK_1_SOVETOV_E_CLOSE_VALUE_VECTOR_CLOSE_VALUE_VECTOR_H_
+
+#include <vector>
+
+std::vector<int> getRandomVector(int raz);
+int getMinDifferenceOneProc(const std::vector<int> wektor);
+int getMinDifference(const std::vector<int> wektor);
+
+#endif  // MODULES_TASK_1_SOVETOV_E_CLOSE_VALUE_VECTOR_CLOSE_VALUE_VECTOR_H_
