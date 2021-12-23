9cfb5dff30ca722490c598ba76d8a7e7c656fe17
ElanskiyAA <sasaelan1@gmail.com>
2021-12-24 00:24:53 +0300

    All Done

---
+++ modules/task_1/elanskiy_a_max_values_in_rows/main.cpp
@@ -0,0 +1,111 @@
+﻿//  Copyright 2021 Elanskiy Akexandr
+#include "./max_values_in_rows.h"
+
+TEST(MPI_TEST, size_1x10) {
+    int procRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    int rows = 1;
+    int columns = 10;
+    std::vector<int> matrix;
+    std::vector<int> res_seq;
+    std::vector<int> res_par;
+    matrix = mat_gen(rows, columns);
+    res_par = max_in_rows_par(matrix, rows, columns);
+    if (procRank == 0) {
+        res_seq = max_in_rows_seq(matrix, columns);
+        ASSERT_EQ(res_seq, res_par);
+    }
+}
+
+TEST(MPI_TEST, size_13x37) {
+    int procRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    int rows = 13;
+    int columns = 37;
+    std::vector<int> matrix;
+    std::vector<int> res_seq;
+    std::vector<int> res_par;
+    matrix = mat_gen(rows, columns);
+    res_par = max_in_rows_par(matrix, rows, columns);
+    if (procRank == 0) {
+        res_seq = max_in_rows_seq(matrix, columns);
+        ASSERT_EQ(res_seq, res_par);
+    }
+}
+
+TEST(MPI_TEST, size_20x20) {
+    int procRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    int rows = 20;
+    int columns = 20;
+    std::vector<int> matrix;
+    std::vector<int> res_seq;
+    std::vector<int> res_par;
+    matrix = mat_gen(rows, columns);
+    res_par = max_in_rows_par(matrix, rows, columns);
+    if (procRank == 0) {
+        res_seq = max_in_rows_seq(matrix, columns);
+        ASSERT_EQ(res_seq, res_par);
+    }
+}
+
+TEST(MPI_TEST, size_20x77) {
+    int procRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    int rows = 20;
+    int columns = 77;
+    std::vector<int> matrix;
+    std::vector<int> res_seq;
+    std::vector<int> res_par;
+    matrix = mat_gen(rows, columns);
+    res_par = max_in_rows_par(matrix, rows, columns);
+    if (procRank == 0) {
+        res_seq = max_in_rows_seq(matrix, columns);
+        ASSERT_EQ(res_seq, res_par);
+    }
+}
+
+TEST(MPI_TEST, size_37x5) {
+    int procRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    int rows = 37;
+    int columns = 5;
+    std::vector<int> matrix;
+    std::vector<int> res_seq;
+    std::vector<int> res_par;
+    matrix = mat_gen(rows, columns);
+    res_par = max_in_rows_par(matrix, rows, columns);
+    if (procRank == 0) {
+        res_seq = max_in_rows_seq(matrix, columns);
+        ASSERT_EQ(res_seq, res_par);
+    }
+}
+
+TEST(MPI_TEST, size_26x11) {
+    int procRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    int rows = 26;
+    int columns = 11;
+    std::vector<int> matrix;
+    std::vector<int> res_seq;
+    std::vector<int> res_par;
+    matrix = mat_gen(rows, columns);
+    res_par = max_in_rows_par(matrix, rows, columns);
+    if (procRank == 0) {
+        res_seq = max_in_rows_seq(matrix, columns);
+        ASSERT_EQ(res_seq, res_par);
+    }
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    std::vector<int> result1;
+    std::vector<int> result2;
+    MPI_Init(&argc, &argv);
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}
---
+++ modules/task_1/elanskiy_a_max_values_in_rows/max_values_in_rows.cpp
@@ -0,0 +1,75 @@
+//  Copyright 2021 Elanskiy Akexandr
+#include "../../../modules/task_1/elanskiy_a_max_values_in_rows/max_values_in_rows.h"
+
+std::vector<int> mat_gen(int rows, int cols) {
+    std::mt19937 gen(time(0));
+    int size = rows * cols;
+    std::vector<int> matrix(size);
+    for (int i = 0; i < size; i++) {
+        matrix[i] = gen() % 100;
+    }
+    return matrix;
+}
+
+std::vector<int> max_in_rows_seq(std::vector<int> m, int cols) {
+    std::vector<int> res;
+    int max;
+    int size = m.size();
+    for (int i = 0; i < size; i += cols) {
+        max = m[i];
+        for (int j = 1; j < cols; j++) {
+            if (m[i + j] > max) {
+                max = m[i + j];
+            }
+        }
+        res.push_back(max);
+    }
+    return res;
+}
+
+std::vector<int> max_in_rows_par(std::vector<int> m, int rows, int cols) {
+    int procNum;
+    int procRank;
+    MPI_Comm_size(MPI_COMM_WORLD, &procNum);
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+
+    int max = 0;
+    std::vector<int> part_size_of_m(procNum);
+    std::vector<int> displs(procNum);
+    int sum = 0;
+    for (int i = 0; i < procNum; i++) {
+        part_size_of_m[i] = rows / procNum * cols;
+        displs[i] = sum;
+        sum += part_size_of_m[i];
+    }
+    if (rows % procNum > 0) {
+        part_size_of_m[procNum - 1] += rows % procNum * cols;
+    }
+    std::vector<int> part_res;
+    std::vector<int> part_of_m(part_size_of_m[procRank]);
+    MPI_Scatterv(m.data(), part_size_of_m.data(), displs.data(), MPI_INT,
+        part_of_m.data(), part_size_of_m[procRank], MPI_INT, 0, MPI_COMM_WORLD);
+
+    for (int i = 0; i < part_size_of_m[procRank]; i += cols) {
+        max = part_of_m[i];
+        for (int j = 1; j < cols; j++) {
+            if (part_of_m[i + j] > max) {
+                max = part_of_m[i + j];
+            }
+        }
+        part_res.push_back(max);
+    }
+    std::vector<int> res(rows);
+    sum = 0;
+    for (int i = 0; i < procNum; i++) {
+        part_size_of_m[i] = rows / procNum;
+        displs[i] = sum;
+        sum += part_size_of_m[i];
+    }
+    if (rows % procNum > 0) {
+        part_size_of_m[procNum - 1] += rows % procNum;
+    }
+    MPI_Gatherv(part_res.data(), part_size_of_m[procRank], MPI_INT,
+        res.data(), part_size_of_m.data(), displs.data(), MPI_INT, 0, MPI_COMM_WORLD);
+    return res;
+}
