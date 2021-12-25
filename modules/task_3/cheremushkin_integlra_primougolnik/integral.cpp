6f78b4f36784ec19e5e71199aed877c9d7e4547b
CheremshkinKI <kenodi.d@mail.ru>
2021-12-25 14:26:21 +0300

    LAST

--- modules/task_3/cheremushkin_integlra_primougolnik/integral.cpp
+++ modules/task_3/cheremushkin_integlra_primougolnik/integral.cpp
@@ -1,6 +1,6 @@
 // Copyright 2021 Cheremushkin Kirill
 #include <mpi.h>
-#include <vector>
+#include <cmath>
 #include "../../../modules/task_3/cheremushkin_integlra_primougolnik/integral.h"
 double linelintegral(const std::vector<std::pair<double, double>>& limits) {
     int i;
@@ -23,11 +23,15 @@ double linelintegral(const std::vector<std::pair<double, double>>& limits) {
             c = 0;
             while (2 != c) {
                 if (i % 2 == 0) {
-                    xb[i] = (static_cast<double>(process_num - process) * x_min
-                    + static_cast<double>(process - 1) * x_max) / static_cast<double>(process_num - 1);
+                    xb[i] = (static_cast<double>(process_num - process)
+                        * x_min
+                    + static_cast<double>(process - 1) * x_max)
+                        / static_cast<double>(process_num - 1);
                 } else {
-                  xb[i] = (static_cast<double>(process_num - process - 1) * x_min
-                  + static_cast<double>(process)*x_max) / static_cast<double>(process_num - 1);
+                  xb[i] = (static_cast<double>(process_num - process - 1)
+                      * x_min
+                  + static_cast<double>(process)*x_max)
+                      / static_cast<double>(process_num - 1);
                 }
                 c++;
                 i++;
@@ -40,44 +44,54 @@ double linelintegral(const std::vector<std::pair<double, double>>& limits) {
     for (i = 1; i <= m; i++) {
         if (xb.size() / 2 == 1) {
             x = (static_cast<double>(2 * m - 2 * i + 1) * xb[0]
-            + static_cast<double>(2 * i - 1) * xb[1]) / static_cast<double>(2 * m);
+            + static_cast<double>(2 * i - 1) * xb[1]) /
+                static_cast<double>(2 * m);
             q_local = q_local + f(x);
         }
         if (xb.size() / 2 == 2) {
             x = (static_cast<double>(2 * m - 2 * i + 1) * xb[0]
-            + static_cast<double>(2 * i - 1) * xb[1]) / static_cast<double>(2 * m);
+            + static_cast<double>(2 * i - 1) * xb[1]) /
+                static_cast<double>(2 * m);
             y = (static_cast<double>(2 * m - 2 * i + 1) * xb[2]
-           + static_cast<double>(2 * i - 1) * xb[3]) / static_cast<double>(2 * m);
+           + static_cast<double>(2 * i - 1) * xb[3]) /
+                static_cast<double>(2 * m);
             mass[0] = x; mass[1] = y;
             q_local = q_local + ff(mass);
         }
         if (xb.size() / 2 == 3) {
             x = (static_cast<double>(2 * m - 2 * i + 1) * xb[0]
-            + static_cast<double>(2 * i - 1) * xb[1]) / static_cast<double>(2 * m);
+            + static_cast<double>(2 * i - 1) * xb[1]) /
+                static_cast<double>(2 * m);
             y = (static_cast<double>(2 * m - 2 * i + 1) * xb[2]
-            + static_cast<double>(2 * i - 1) * xb[3]) / static_cast<double>(2 * m);
+            + static_cast<double>(2 * i - 1) * xb[3]) /
+                static_cast<double>(2 * m);
             z = (static_cast<double>(2 * m - 2 * i + 1) * xb[4]
-            + static_cast<double>(2 * i - 1) * xb[5]) / static_cast<double>(2 * m);
+            + static_cast<double>(2 * i - 1) * xb[5]) /
+                static_cast<double>(2 * m);
             mass[0] = x; mass[1] = y; mass[2] = z;
             q_local = q_local + fff(mass);
         }
     }
     if (xb.size() / 2 == 1) {
-        q_local = q_local * (xb[1] - xb[0]) / static_cast<double>(m);
+        q_local = q_local * (xb[1] - xb[0]) /
+            static_cast<double>(m);
     }
     if (xb.size() / 2 == 2) {
         q_local = q_local * (xb[1] - xb[0]) * (xb[3] - xb[2])
         / static_cast<double>(m) / static_cast<double>(m);
     }
     if (xb.size() / 2 == 3) {
-        q_local = q_local * (xb[1] - xb[0]) / static_cast<double>(m) * (xb[3] - xb[2])
-        / static_cast<double>(m) * (xb[5] - xb[4]) / static_cast<double>(m);
+        q_local = q_local * (xb[1] - xb[0]) /
+            static_cast<double>(m) * (xb[3] - xb[2])
+        / static_cast<double>(m) * (xb[5] - xb[4]) /
+            static_cast<double>(m);
     }
     q_global = 0.0;
     q_global = q_global + q_local;
     return q_global - 0.624;
 }
-double Parallelintegral(const std::vector<std::pair<double, double>>& limits) {
+double Parallelintegral(const std::vector<std::pair<double,
+    double>>& limits) {
     int i;
     int m;
     int master = 0;
@@ -115,13 +129,18 @@ double Parallelintegral(const std::vector<std::pair<double, double>>& limits) {
                 c = 0;
                 while (2 != c) {
                     if (i % 2 == 0) {
-                       xb[i] = (static_cast<double>(process_num - process) * x_min
-                       + static_cast<double>(process - 1) * x_max) / static_cast<double>(process_num - 1);
+                       xb[i] = (static_cast<double>(process_num - process)
+                          * x_min
+                       + static_cast<double>(process - 1) * x_max) /
+                           static_cast<double>(process_num - 1);
                     } else {
-                      xb[i] = (static_cast<double>(process_num - process - 1) * x_min
-                      + static_cast<double>(process)*x_max) / static_cast<double>(process_num - 1);
+                      xb[i] = (static_cast<double>(process_num - process - 1)
+                          * x_min
+                      + static_cast<double>(process)*x_max) /
+                          static_cast<double>(process_num - 1);
                     }
-                    MPI_Send(&xb[i], 2, MPI_DOUBLE, target, tag, MPI_COMM_WORLD);
+                    MPI_Send(&xb[i], 2, MPI_DOUBLE, target,
+                        tag, MPI_COMM_WORLD);
                     c++;
                     i++;
                 }
@@ -131,7 +150,8 @@ double Parallelintegral(const std::vector<std::pair<double, double>>& limits) {
         source = master;
         tag = 1;
         for (size_t i = 0; i < xb.size(); i++) {
-             MPI_Recv(&xb[i], 2, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &status);
+             MPI_Recv(&xb[i], 2, MPI_DOUBLE, source,
+                 tag, MPI_COMM_WORLD, &status);
         }
     }
     MPI_Barrier(MPI_COMM_WORLD);
@@ -144,30 +164,37 @@ double Parallelintegral(const std::vector<std::pair<double, double>>& limits) {
         for (i = 1; i <= m; i++) {
             if (xb.size() / 2 == 1) {
                 x = (static_cast<double>(2 * m - 2 * i + 1) * xb[0]
-                + static_cast<double>(2 * i - 1) * xb[1]) / static_cast<double>(2 * m);
+                + static_cast<double>(2 * i - 1) * xb[1]) /
+                    static_cast<double>(2 * m);
                 q_local = q_local + f(x);
             }
             if (xb.size() / 2 == 2) {
                 x = (static_cast<double>(2 * m - 2 * i + 1) * xb[0]
-                + static_cast<double>(2 * i - 1) * xb[1]) / static_cast<double>(2 * m);
+                + static_cast<double>(2 * i - 1) * xb[1]) /
+                    static_cast<double>(2 * m);
                 y = (static_cast<double>(2 * m - 2 * i + 1) * xb[2]
-                + static_cast<double>(2 * i - 1) * xb[3]) / static_cast<double>(2 * m);
+                + static_cast<double>(2 * i - 1) * xb[3]) /
+                    static_cast<double>(2 * m);
                 mass[0] = x; mass[1] = y;
                 q_local = q_local + ff(mass);
             }
             if (xb.size() / 2 == 3) {
                 x = (static_cast<double>(2 * m - 2 * i + 1) * xb[0]
-                + static_cast<double>(2 * i - 1) * xb[1]) / static_cast<double>(2 * m);
+                + static_cast<double>(2 * i - 1) * xb[1]) /
+                    static_cast<double>(2 * m);
                 y = (static_cast<double>(2 * m - 2 * i + 1) * xb[2]
-                + static_cast<double>(2 * i - 1) * xb[3]) / static_cast<double>(2 * m);
+                + static_cast<double>(2 * i - 1) * xb[3]) /
+                    static_cast<double>(2 * m);
                 z = (static_cast<double>(2 * m - 2 * i + 1) * xb[4]
-                + static_cast<double>(2 * i - 1) * xb[5]) / static_cast<double>(2 * m);
+                + static_cast<double>(2 * i - 1) * xb[5]) /
+                    static_cast<double>(2 * m);
                 mass[0] = x; mass[1] = y; mass[2] = z;
                 q_local = q_local + fff(mass);
             }
         }
         if (xb.size() / 2 == 1) {
-            q_local = q_local * (xb[1] - xb[0]) / static_cast<double>(m);
+            q_local = q_local * (xb[1] - xb[0]) /
+                static_cast<double>(m);
         }
         if (xb.size() / 2 == 2) {
             q_local = q_local * (xb[1] - xb[0]) * (xb[3] - xb[2])
@@ -175,7 +202,8 @@ double Parallelintegral(const std::vector<std::pair<double, double>>& limits) {
         }
         if (xb.size() / 2 == 3) {
             q_local = q_local * (xb[1] - xb[0]) / static_cast<double>(m)
-            * (xb[3] - xb[2]) / static_cast<double>(m) * (xb[5] - xb[4]) / static_cast<double>(m);
+            * (xb[3] - xb[2]) / static_cast<double>(m)
+                * (xb[5] - xb[4]) / static_cast<double>(m);
         }
         target = master;
         tag = 2;
@@ -186,7 +214,8 @@ double Parallelintegral(const std::vector<std::pair<double, double>>& limits) {
         while (received < process_num - 1) {
             source = MPI_ANY_SOURCE;
             tag = 2;
-            MPI_Recv(&q_local, 1, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &status);
+            MPI_Recv(&q_local, 1, MPI_DOUBLE, source,
+                tag, MPI_COMM_WORLD, &status);
             q_global = q_global + q_local;
             received = received + 1;
         }
