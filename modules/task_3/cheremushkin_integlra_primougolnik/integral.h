abbc9dd9c22b16947db4bb63d5f65421dbd49533
CheremshkinKI <kenodi.d@mail.ru>
2021-12-25 14:00:02 +0300

    ddd

--- modules/task_3/cheremushkin_integlra_primougolnik/integral.h
+++ modules/task_3/cheremushkin_integlra_primougolnik/integral.h
@@ -3,9 +3,10 @@
 #define MODULES_TASK_3_CHEREMUSHKIN_INTEGLRA_PRIMOUGOLNIK_INTEGRAL_H_
 
 #include <stdlib.h>
+#include "math.h"
 #include <utility>
 #include <vector>
-#include "math.h"
+
 
 const double eps = 0.1;
 double linelintegral(const std::vector<std::pair<double, double>>& limits);
