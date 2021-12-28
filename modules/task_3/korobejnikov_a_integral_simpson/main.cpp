10f8893197ca56f0c9bbf1403a12475d8d8bc475
KorobejnikovAP <armok2001@mail.ru>
2021-12-28 16:35:11 +0300

    [fix] fix style

--- modules/task_3/korobejnikov_a_integral_simpson/main.cpp
+++ modules/task_3/korobejnikov_a_integral_simpson/main.cpp
@@ -112,44 +112,44 @@ TEST(MultipleIntegraion, Integral_with_3_dimension) {
 }
 
 TEST(MultipleIntegraion, Integral_with_3_dimension_and_use_log_function) {
-    //int rank;
-    //MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    //int n = 3;
-    //std::vector<std::pair<double, double>> dist(n);
-    //std::vector<int> distr(n);
-    //if (rank == 0) {
-    //    dist[0] = { 1, 2 }; distr[0] = 100;
-    //    dist[1] = { -13, 5 }; distr[1] = 10;
-    //    dist[2] = { 3, 7 }; distr[2] = 10;
-    //}
-    //double result = ParallelVersion(func3, dist, distr);
-    //if (rank == 0) {
-    //    double seq = SequentialVersion(func3, dist, distr);
-    //    double error = 0.0001;
-    //    ASSERT_NEAR(result, seq, error);
-    //}
+    // int rank;
+    // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // int n = 3;
+    // std::vector<std::pair<double, double>> dist(n);
+    // std::vector<int> distr(n);
+    // if (rank == 0) {
+    //     dist[0] = { 1, 2 }; distr[0] = 100;
+    //     dist[1] = { -13, 5 }; distr[1] = 10;
+    //     dist[2] = { 3, 7 }; distr[2] = 10;
+    // }
+    // double result = ParallelVersion(func3, dist, distr);
+    // if (rank == 0) {
+    //     double seq = SequentialVersion(func3, dist, distr);
+    //     double error = 0.0001;
+    //     ASSERT_NEAR(result, seq, error);
+    // }
     ASSERT_EQ(1, 1);
 }
 
 TEST(MultipleIntegraion,
         Integral_with_3_dimension_and_use_sin_and_cos_functions) {
-    //int rank;
-    //MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    //int n = 3;
-    //std::vector<std::pair<double, double>> dist(n);
-    //std::vector<std::pair<double, double>> dist_seq(n);
-    //std::vector<int> distr(n);
-    //if (rank == 0) {
-    //    dist[0] = { -9, 1 }; distr[0] = 100;
-    //    dist[1] = { -100, 100 }; distr[1] = 10;
-    //    dist[2] = { -2, 2 }; distr[2] = 10;
-    //}
-    //double result = ParallelVersion(func4, dist, distr);
-    //if (rank == 0) {
-    //    double seq = SequentialVersion(func4, dist, distr);
-    //    double error = 0.0001;
-    //    ASSERT_NEAR(result, seq, error);
-    //}
+    // int rank;
+    // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // int n = 3;
+    // std::vector<std::pair<double, double>> dist(n);
+    // std::vector<std::pair<double, double>> dist_seq(n);
+    // std::vector<int> distr(n);
+    // if (rank == 0) {
+    //     dist[0] = { -9, 1 }; distr[0] = 100;
+    //     dist[1] = { -100, 100 }; distr[1] = 10;
+    //     dist[2] = { -2, 2 }; distr[2] = 10;
+    // }
+    // double result = ParallelVersion(func4, dist, distr);
+    // if (rank == 0) {
+    //     double seq = SequentialVersion(func4, dist, distr);
+    //     double error = 0.0001;
+    //     ASSERT_NEAR(result, seq, error);
+    // }
     ASSERT_EQ(1, 1);
 }
 
