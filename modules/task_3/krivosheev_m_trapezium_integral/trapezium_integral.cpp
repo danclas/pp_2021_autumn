1c1d39e09b56cb957b93a5053fd0bb94c3779f42
Miron Krivosheev <miron.student2001@bk.ru>
2021-12-28 22:11:16 +0300

    please, build it...

--- modules/task_3/krivosheev_m_trapezium_integral/trapezium_integral.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2021 Krivosheev Miron
-
-#include "../../../modules/task_3/krivosheev_m_trapezium_integral/trapezium_integral.h"
-#include <mpi.h>
-#include <algorithm>
-#include <cctype>
-#include <iostream>
-#include <random>
