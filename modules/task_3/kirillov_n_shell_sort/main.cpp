b4165f1d4c4ee3c9fdbb0366f09ef960af32df44
ZILLeF13 <71269813+ZILLeF13@users.noreply.github.com>
2021-12-29 23:15:50 +0300

    change

--- modules/task_3/kirillov_n_shell_sort/main.cpp
+++ modules/task_3/kirillov_n_shell_sort/main.cpp
@@ -5,24 +5,24 @@
 
 #include "./shell_sort.h"
 
-TEST(Parallel_Operations_MPI, test_sequential) {
+TEST(Parallel_Operations_MPI, test_sequential_size_8) {
   int rank;
-  int size = 10;
+  int size = 8;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   if (rank == 0) {
     int vec[] = {5, 7, 2, 4, 8, 3, 1, 6};
     int vec_res[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    getSequentialOperations(vec, 8);
-    for (int i = 0; i < 8; i++) {
+    getSequentialOperations(vec, size);
+    for (int i = 0; i < size; i++) {
       ASSERT_EQ(vec[i], vec_res[i]);
     }
   }
 }
 
-TEST(Parallel_Operations_MPI, test_sequential_and_parallel) {
+TEST(Parallel_Operations_MPI, test_sequential_and_parallel_size_16) {
   int rank;
   double s, f, t_seq, t_par;
-  int size = 10;
+  int size = 16;
   int* vec = 0;
   int* _vec = 0;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -30,7 +30,9 @@ TEST(Parallel_Operations_MPI, test_sequential_and_parallel) {
     vec = new int[size];
     _vec = new int[size];
     getRandomVector(vec, size);
-    _vec = vec;
+    for (int i = 0; i < size; i++) {
+      _vec[i] = vec[i];
+    }
     s = MPI_Wtime();
     getSequentialOperations(vec, size);
     f = MPI_Wtime();
@@ -50,10 +52,10 @@ TEST(Parallel_Operations_MPI, test_sequential_and_parallel) {
   }
 }
 
-TEST(Parallel_Operations_MPI, test_sequential_and_parallel_size_1000) {
+TEST(Parallel_Operations_MPI, test_sequential_and_parallel_size_128) {
   int rank;
   double s, f, t_seq, t_par;
-  int size = 1000;
+  int size = 128;
   int* vec = 0;
   int* _vec = 0;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -61,7 +63,9 @@ TEST(Parallel_Operations_MPI, test_sequential_and_parallel_size_1000) {
     vec = new int[size];
     _vec = new int[size];
     getRandomVector(vec, size);
-    _vec = vec;
+    for (int i = 0; i < size; i++) {
+      _vec[i] = vec[i];
+    }
     s = MPI_Wtime();
     getSequentialOperations(vec, size);
     f = MPI_Wtime();
@@ -81,10 +85,10 @@ TEST(Parallel_Operations_MPI, test_sequential_and_parallel_size_1000) {
   }
 }
 
-TEST(Parallel_Operations_MPI, test_sequential_and_parallel_size_1501) {
+TEST(Parallel_Operations_MPI, test_sequential_and_parallel_size_512) {
   int rank;
   double s, f, t_seq, t_par;
-  int size = 1501;
+  int size = 512;
   int* vec = 0;
   int* _vec = 0;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -92,7 +96,42 @@ TEST(Parallel_Operations_MPI, test_sequential_and_parallel_size_1501) {
     vec = new int[size];
     _vec = new int[size];
     getRandomVector(vec, size);
-    _vec = vec;
+    for (int i = 0; i < size; i++) {
+      _vec[i] = vec[i];
+    }
+    s = MPI_Wtime();
+    getSequentialOperations(vec, size);
+    f = MPI_Wtime();
+    t_seq = f - s;
+    s = MPI_Wtime();
+  }
+  getParallelOperations(_vec, size);
+  if (rank == 0) {
+    f = MPI_Wtime();
+    t_par = f - s;
+    std::cout << "Time parallel " << t_par << std::endl;
+    std::cout << "Time sequential " << t_seq << std::endl;
+    std::cout << "Boost " << t_seq / t_par << std::endl;
+    for (int i = 0; i < size; i++) {
+      ASSERT_EQ(vec[i], _vec[i]);
+    }
+  }
+}
+
+TEST(Parallel_Operations_MPI, test_sequential_and_parallel_size_2048) {
+  int rank;
+  double s, f, t_seq, t_par;
+  int size = 2048;
+  int* vec = 0;
+  int* _vec = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if (rank == 0) {
+    vec = new int[size];
+    _vec = new int[size];
+    getRandomVector(vec, size);
+    for (int i = 0; i < size; i++) {
+      _vec[i] = vec[i];
+    }
     s = MPI_Wtime();
     getSequentialOperations(vec, size);
     f = MPI_Wtime();
--- modules/task_3/kirillov_n_shell_sort/shell_sort.cpp
+++ modules/task_3/kirillov_n_shell_sort/shell_sort.cpp
@@ -15,6 +15,7 @@ void getRandomVector(int* vec, int size) {
 
 void getSequentialOperations(int* vec, int size) {
   if (size < 1) throw -1;
+
   for (int step = size / 2; step > 0; step /= 2)
     for (int i = step; i < size; i++) {
       int tmp = vec[i];
@@ -32,10 +33,11 @@ void getSequentialOperations(int* vec, int size) {
 void getParallelOperations(int* vec, int size_vec) {
   if (size_vec < 1) throw -1;
   int rank, size;
-  int* res;
-  int* _vec;
-  int step = 1;
+  int* res = 0;
+  int* _vec = 0;
+  int step;
   MPI_Status status;
+  int size_n = size_vec, size_m = size_vec;
 
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -45,8 +47,8 @@ void getParallelOperations(int* vec, int size_vec) {
 
   if (rank == 0) {
     if (remains != 0) {
-      vec = new int[size_vec + size - remains];
-      for (int i = size_vec; i < size_vec + size - remains; i++) vec[i] = 0;
+      vec = new int[size_n + size - remains];
+      for (int i = size_n; i < size_n + size - remains; i++) vec[i] = 0;
       local_size = local_size + 1;
     }
 
@@ -62,19 +64,21 @@ void getParallelOperations(int* vec, int size_vec) {
                 MPI_COMM_WORLD);
     getSequentialOperations(res, local_size);
   }
-
+  step = 1;
   while (step < size) {
     if (rank % (2 * step) == 0) {
       if (rank + step < size) {
-        MPI_Recv(&size_vec, 1, MPI_INT, rank + step, 0, MPI_COMM_WORLD,
+        MPI_Recv(&size_m, 1, MPI_INT, rank + step, 0, MPI_COMM_WORLD, &status);
+        _vec = new int[size_m];
+        MPI_Recv(_vec, size_m, MPI_INT, rank + step, 0, MPI_COMM_WORLD,
                  &status);
-        _vec = new int[size_vec];
-        MPI_Recv(_vec, size_vec, MPI_INT, rank + step, 0, MPI_COMM_WORLD,
-                 &status);
-        int* _res = res;
-        res = new int[local_size + size_vec];
+        int* _res = new int[local_size];
+        for (int i = 0; i < local_size; i++) {
+          _res[i] = res[i];
+        }
+        res = new int[local_size + size_m];
         int i = 0, j = 0, ij = 0;
-        while (i < local_size && j < size_vec)
+        while (i < local_size && j < size_m)
           if (_res[i] < _vec[j]) {
             res[ij] = _res[i];
             i++;
@@ -85,7 +89,7 @@ void getParallelOperations(int* vec, int size_vec) {
             ij++;
           }
         if (i == local_size) {
-          while (j < size_vec) {
+          while (j < size_m) {
             res[ij] = _vec[j];
             j++;
             ij++;
@@ -98,7 +102,7 @@ void getParallelOperations(int* vec, int size_vec) {
           }
         }
         delete[] _res;
-        local_size = local_size + size_vec;
+        local_size = local_size + size_m;
       }
     } else {
       MPI_Send(&local_size, 1, MPI_INT, rank - step, 0, MPI_COMM_WORLD);
@@ -108,11 +112,8 @@ void getParallelOperations(int* vec, int size_vec) {
     step = step * 2;
   }
   if (rank == 0) {
-    vec = res;
-    if (size_vec % size) {
-      delete[] vec;
-      vec = new int[size_vec];
-      vec = res;
+    for (int i = 0; i < size_vec; i++) {
+      vec[i] = res[i];
     }
   }
 }
