1f0ab4e8923f7a9357a48f22921300db5168e804
AntonPoplavsky <poplavskyanton@mail.ru>
2021-12-29 23:44:14 +0300

    fff

--- modules/task_3/poplavskiy_a_monte_carlo/monte_carlo.cpp
+++ modules/task_3/poplavskiy_a_monte_carlo/monte_carlo.cpp
@@ -3,20 +3,20 @@
 
 double getSequentialIntegral(vector<array<double, 2>> a_b, int points,
                              function<double(vector<double>)> f) {
-  int dimension = static_cast<int>(a_b.size());
+  int dimension = static_cast<int>(a_b.size());  
   mt19937 gen(time(0));
   vector<uniform_real_distribution<double>> uniform(dimension);
 
   double result = 0.0;
 
   for (int i = 0; i < dimension; i++)
-    uniform[i] = uniform_real_distribution<double>(a_b[i][0], a_b[i][1]);
+    uniform[i] = uniform_real_distribution<double>(a_b[i][0], a_b[i][1]); 
 
-  vector<double> node(dimension);
-
+  vector<double> node(dimension);  
+   
   for (int i = 0; i < points; i++) {
-    for (int j = 0; j < dimension; j++) node[j] = uniform[j](gen);
-    result += f(node);
+    for (int j = 0; j < dimension; j++) node[j] = uniform[j](gen);  
+    result += f(node);                                              
   }
   for (int i = 0; i < dimension; i++) {
     result *= (a_b[i][0] - a_b[i][1]);
@@ -59,7 +59,7 @@ double getParallelIntegral(vector<array<double, 2>> a_b, int points_num,
 
   vector<double> func_params(integral_dim);
 
-  for (int i = 0; i < delta; i++) {
+  for (int i = 0; i < delta; i++) { 
     for (int j = 0; j < integral_dim; j++) {
       func_params[j] = local_points[(i * integral_dim) + j];
     }
