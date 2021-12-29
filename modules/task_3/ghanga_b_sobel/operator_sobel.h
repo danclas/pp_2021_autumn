ec29999f5e45bebb51ff6f98d85aa5b4bc2e9ba5
Владимир Бутеску <vbutescu2000@gmail.com>
2021-12-29 19:38:21 +0300

    V1

---
+++ modules/task_3/ghanga_b_sobel/operator_sobel.h
@@ -0,0 +1,11 @@
+// Copyright 2021 ghanga moukela brexe juniors
+#ifndef MODULES_TASK_3_GHANGA_B_OPERATOR_SOBEL_OPERATOR_SOBEL_H_
+#define MODULES_TASK_3_GHANGA_B_OPERATOR_SOBEL_OPERATOR_SOBEL_H_
+#include <vector>
+
+int clamp(int v, int max, int min);
+std::vector<int> calc_sobel(std::vector<int> image, int rows, int cols);
+int calc_kernel(std::vector<int> image, int x, int y, int rows, int cols);
+std::vector<int> parallel_sobel(std::vector<int> image, int rows, int cols);
+
+#endif  // MODULES_TASK_3_GHANGA_B_OPERATOR_SOBEL_OPERATOR_SOBEL_H_
