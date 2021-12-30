ff6e5dc09d104ec061a48d4da8b65c2371f58c14
brimst0ne <artemchelebaev@gmail.com>
2021-12-30 10:07:31 +0300

    Added a test

--- modules/task_3/chelebaev_strassen_algorithm/main.cpp
+++ modules/task_3/chelebaev_strassen_algorithm/main.cpp
@@ -9,16 +9,23 @@ TEST(Multiply, Matrix16) {
     MPI_Comm_size(MPI_COMM_WORLD, &tasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     Matrix a, b;
+    double stime, ptime;
     if (my_rank == 0) {
         int h = 16, w = 16;
         double max_num = 10.0;
         a = createRandomMatrix(h, w, max_num);
         b = createRandomMatrix(h, w, max_num);
     }
+    clock_t startp = clock();
     Matrix res = parallelMulti(a, b);
+    ptime = (clock() - startp) / static_cast<double>(CLOCKS_PER_SEC);
     if (my_rank == 0) {
         double delta = 0.1;
+        clock_t starts = clock();
         Matrix seq = sequentialMulti(a, b);
+        stime = (clock() - starts) / static_cast<double>(CLOCKS_PER_SEC);
+        std::cout << "Sequential time: " << stime << std::endl;
+        std::cout << "Parallel time: " << ptime << std::endl;
         ASSERT_TRUE(areEqual(seq, res, delta));
     }
 }
@@ -83,6 +90,32 @@ TEST(Multiply, Matrix128) {
     double stime, ptime;
     if (my_rank == 0) {
         int n = 128, m = 128;
+        double max_num = 100.0;
+        a = createRandomMatrix(n, m, max_num);
+        b = createRandomMatrix(n, m, max_num);
+    }
+    clock_t startp = clock();
+    Matrix res = parallelMulti(a, b);
+    ptime = (clock() - startp) / static_cast<double>(CLOCKS_PER_SEC);
+    if (my_rank == 0) {
+        double delta = 0.1;
+        clock_t starts = clock();
+        Matrix seq = sequentialMulti(a, b);
+        stime = (clock() - starts) / static_cast<double>(CLOCKS_PER_SEC);
+        std::cout << "Sequential time: " << stime << std::endl;
+        std::cout << "Parallel time: " << ptime << std::endl;
+        ASSERT_TRUE(areEqual(seq, res, delta));
+    }
+}
+
+TEST(Multiply, Matrix256) {
+    int tasks, my_rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &tasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    Matrix a, b;
+    double stime, ptime;
+    if (my_rank == 0) {
+        int n = 256, m = 256;
         double max_num = 100.0;
         a = createRandomMatrix(n, m, max_num);
         b = createRandomMatrix(n, m, max_num);
